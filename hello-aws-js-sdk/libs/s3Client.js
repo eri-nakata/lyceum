aedde96963be05ff6f291b40779a30cd71539822
eri-nakata <149447168+eri-nakata@users.noreply.github.com>
2024-01-24 15:47:11 +0900

    Update s3Client.js

--- hello-aws-js-sdk/libs/s3Client.js
+++ hello-aws-js-sdk/libs/s3Client.js
@@ -15,7 +15,8 @@ Inputs (replace in code):
 import { S3Client } from "@aws-sdk/client-s3";
 
 // Set the AWS Region.
-const REGION = "ap-northeast-1"; //e.g. "us-east-1"
+//const REGION = "ap-northeast-1"; //e.g. "us-east-1"
+const REGION = "eu-west-2"; //ロンドン
 // Create an Amazon S3 service client object.
 const s3Client = new S3Client({ region: REGION });
 export { s3Client };
